4f1c2605de837d7afc6ba521221b64b5829245fc
Vika Putri Ariyanti <vikaputriariyanti@gmail.com>
2022-01-27 10:11:21 +0700

    more button modif

--- frontend/src/components/Query/index.js
+++ frontend/src/components/Query/index.js
@@ -2,13 +2,23 @@ import React from "react";
 import { useQuery } from "@apollo/react-hooks";
 
 const Query = ({ children, query, slug, name }) => {
-  const { data, loading, error } = useQuery(query, {
+  const { data, loading, error, fetchMore  } = useQuery(query, {
     variables: { slug: slug, name: name }
   });
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {JSON.stringify(error)}</p>;
-  return children({ data });
+  if (loading) return (
+  	<div className="mx-3 px-5">
+  		<p className="mx-4 px-5">Loading</p>
+  	</div>
+  );
+
+  if (error) return (
+  	<div className="mx-3 px-5">
+  		<p className="mx-4 px-5">Artikel tidak tersedia</p>
+  	</div>
+  )
+
+  return children({ data, fetchMore });
 };
 
 export default Query;
--- frontend/src/containers/Articles/index.js
+++ frontend/src/containers/Articles/index.js
@@ -3,38 +3,8 @@ import Articles from "../../components/Articles";
 import Query from "../../components/Query";
 import ARTICLES_QUERY from "../../queries/article/articles";
 import { Link } from "react-router-dom";
-import { useQuery } from '@apollo/client';
 
 const Home = () => {
-  const { error, data, loading, fetchMore } = useQuery(ARTICLES_QUERY)
-  
-  if (error) {
-    console.error(error);
-    return (
-      <header className="py-5 px-3">
-        <div className="container px-5">
-          <h1 className="py-2">
-            <Link className="text-decoration-none" to="/">Blog</Link>
-          </h1>
-          <p className="py-2">Artikel tidak tersedia</p>
-        </div>
-      </header>
-    );
-  }
-
-  if (loading) {
-    return (
-      <header className="py-5 px-3">
-        <div className="container px-5">
-          <h1 className="py-2">
-            <Link className="text-decoration-none" to="/">Blog</Link>
-          </h1>
-          <p className="py-2">Loading</p>
-        </div>
-      </header>
-    );
-  }
-
   return (
     <header className="py-5 px-3">
       <div className="container px-5">
@@ -50,32 +20,36 @@ const Home = () => {
       </div>
 
       <Query query={ARTICLES_QUERY}>
-        {({ data: { articles } }) => {
-          return <Articles articles={articles} />
+        {({ data: { articles }, fetchMore  }) => {
+          return (
+            <div>
+              <Articles articles={articles} />
+
+              <div className="container px-5">
+                {articles.pageInfo.hasNextPage && (<button
+                  className="btn btn-primary"
+                    onClick={() => {
+                      const { endCursor } = articles.pageInfo;
+                      fetchMore({
+                        variables: { after: endCursor },
+                        updateQuery: (prevResult, { fetchMoreResult }) => {
+                          fetchMoreResult.articles.edges = [
+                            ...prevResult.articles.edges,
+                            ...fetchMoreResult.articles.edges
+                          ];
+                          return fetchMoreResult;
+                        }
+                      });
+                    }}
+                    >Lebih Banyak
+                      <i className="fas fa-sync ms-2"></i>
+                    </button>
+                )}
+              </div>
+            </div>
+          )
         }}
       </Query>
-
-      <div className="container px-5">
-        {data.articles.pageInfo.hasNextPage && (<button
-          className="btn btn-primary"
-            onClick={() => {
-              const { endCursor } = data.articles.pageInfo;
-              fetchMore({
-                variables: { after: endCursor },
-                updateQuery: (prevResult, { fetchMoreResult }) => {
-                  fetchMoreResult.articles.edges = [
-                    ...prevResult.articles.edges,
-                    ...fetchMoreResult.articles.edges
-                  ];
-                  return fetchMoreResult;
-                }
-              });
-            }}
-          >Lebih Banyak
-          </button>
-        )}
-      </div>
-      
     </header>
   );
 };
