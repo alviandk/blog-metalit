dddd2b8904966a5651052796b8c20ec9d12e82d2
Vika Putri Ariyanti <vikaputriariyanti@gmail.com>
2022-01-27 10:14:55 +0700

    more button modif

--- frontend/src/containers/Category/index.js
+++ frontend/src/containers/Category/index.js
@@ -8,40 +8,10 @@ import { useQuery } from '@apollo/client';
 
 const Category = () => {
   let { id } = useParams();
-  const { error, data, loading, fetchMore } = useQuery(CATEGORY_ARTICLES_QUERY, {
-    variables: { slug: id }
-  })
-
-  if (error) {
-    console.error(error);
-    return (
-      <header className="py-5 px-3">
-        <div className="container px-5">
-          <h1 className="py-2">
-            <Link className="text-decoration-none" to="/">Blog</Link>
-          </h1>
-          <p className="py-2">Artikel tidak tersedia</p>
-        </div>
-      </header>
-    );
-  }
-
-  if (loading) {
-    return (
-      <header className="py-5 px-3">
-        <div className="container px-5">
-          <h1 className="py-2">
-            <Link className="text-decoration-none" to="/">Blog</Link>
-          </h1>
-          <p className="py-2">Loading</p>
-        </div>
-      </header>
-    );
-  }
 
   return (
     <Query query={CATEGORY_ARTICLES_QUERY} slug={id}>
-      {({ data: { categories } }) => {
+      {({ data: { categories }, fetchMore}) => {
           return (
             <header className="py-5 px-2">
               <div className="container px-5">
@@ -58,26 +28,26 @@ const Category = () => {
               </div>
               <Articles articles={categories.article} />
 
-                   <div className="container px-5">
-      {data.categories.article.pageInfo.hasNextPage && (<button
-        className="btn btn-primary"
-          onClick={() => {
-            const { endCursor } = data.categories.article.pageInfo;
-            fetchMore({
-              variables: { after: endCursor },
-              updateQuery: (prevResult, { fetchMoreResult }) => {
-                fetchMoreResult.categories.article.edges = [
-                  ...prevResult.categories.article.edges,
-                  ...fetchMoreResult.categories.article.edges
-                ];
-                return fetchMoreResult;
-              }
-            });
-          }}
-        >Lebih Banyak
-        </button>
-      )}
-      </div>
+              <div className="container px-5">
+                {categories.article.pageInfo.hasNextPage && (<button
+                  className="btn btn-primary"
+                    onClick={() => {
+                      const { endCursor } = categories.article.pageInfo;
+                      fetchMore({
+                        variables: { after: endCursor },
+                        updateQuery: (prevResult, { fetchMoreResult }) => {
+                          fetchMoreResult.categories.article.edges = [
+                            ...prevResult.categories.article.edges,
+                            ...fetchMoreResult.categories.article.edges
+                          ];
+                          return fetchMoreResult;
+                        }
+                      });
+                    }}
+                  >Lebih Banyak
+                  </button>
+                )}
+              </div>
             </header>
           );
       }}
